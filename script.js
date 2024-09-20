5f1176397cf3ec75775e1c38752f046eb30ab785
Santi <santiagofrau3@gmail.com>
2024-09-20 14:44:28 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,4 @@
+//<h1 class="titulo">Estas flores amarillas son un reflejo de la alegría que traes a mi vida. <br> <br>
+//Gracias por iluminar mis días con tu presencia.</h1>
+
+// cambiar flower__leaf es la flor la rama la dejo igual
